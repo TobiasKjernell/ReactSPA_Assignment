fae9fd55e3023245f1ec7083ed9ee7652b6a904f
DenVitaKaninen <tobiaskjernell@gmail.com>
2025-03-23 02:30:03 +0100

    updated for mobile

--- src/components/NavigationItem/index.jsx
+++ src/components/NavigationItem/index.jsx
@@ -1,7 +1,7 @@
 import styles from './navItem.module.css'
 const NavigationItem = ({ category, onCurrentPage, currentPage }) => {
     
-    return <div className={`${styles.navItem} ${styles[category === currentPage ? "active" : ""]}`} 
+    return <div className={`${styles.navItem} ${styles[category === currentPage ? "active" : "unactive"]}`} 
     onClick={() => onCurrentPage(category)}>{category}</div>
 }
 export default NavigationItem;
